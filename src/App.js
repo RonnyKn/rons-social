180fc4c5c860155c8d155bb5337dcf488abd38a2
RonnyKn <ronnykurniawn@gmail.com>
2023-01-16 23:34:04 +0700

    adding Login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import "./App.css"
+import Login from "./pages/login/Login"
+import Register from "./pages/register/Register"
 
 function App() {
   return (
     <>
-      <p>hello</p>
+      <Login />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/login" elemment={<Login />} />
+          <Route path="/register" elemment={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -2,7 +2,30 @@ import "./Login.css"
 import React from "react"
 
 const Login = () => {
-  return <div>Login</div>
+  return (
+    <div className="login">
+      <div className="login-left">
+        <h1>Hello World</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus minima
+          aliquid, nobis tempora autem ratione ducimus mollitia nesciunt
+          veritatis pariatur?
+        </p>
+        <p>Do you have an account?</p>
+        <button className="btn-sign">Register</button>
+      </div>
+      <div className="login-right">
+        <h2>Login</h2>
+        <form className="login-form">
+          <label>Username:</label>
+          <input type="text" />
+          <label>Password:</label>
+          <input type="password" />
+        </form>
+        <button className="btn-sign">Login</button>
+      </div>
+    </div>
+  )
 }
 
 export default Login
